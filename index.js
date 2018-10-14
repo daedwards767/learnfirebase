a46114c1ab41ecd38a6a571e68cbe739435ba2b0
Davidson Edwards <davidson@scape.io>
2018-10-14 20:15:30 +0100

    added form and db push function to write to the database

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+var db
 window.onload = function(){
-var ref = firebase.database().ref('test-earn-caribbean');
+db = firebase.database().ref('applicants');
 
-console.log(ref);
+console.log(db);
+
+db.once("value")
+  .then(function(snapshot) {
+    var applicant = snapshot.child("1").val();
+    console.log(applicant)
+  })
+}
+
+function addApplicant (){
+    var firstName = document.getElementById("firstName").value
+    var lastName = document.getElementById("lastName").value
+    try{
+        db.push({
+            firstName: firstName,
+            lastName: lastName,
+        }, function() {
+            var success = firstName + " " + lastName + " has been successfully added."
+            console.log(success)
+            clearFields()
+        })
+        
+    }
+    catch(e){
+        console.warn("Error while adding applicant " + firstName + " " + lastName + "\n" + e)
+    }
+}
+
+function clearFields(){
+    document.getElementById("firstName").value = null
+    document.getElementById("lastName").value = null
 }
